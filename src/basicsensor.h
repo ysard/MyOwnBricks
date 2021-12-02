4cdf6e785eebc508b8ad349807dae1b77ccbb6f1
ysard <ysard@users.noreply.github.com>
2021-12-02 03:44:28 +0100

    Add missing license text

--- src/basicsensor.h
+++ src/basicsensor.h
@@ -1,3 +1,22 @@
+/*
+ * A library for the emulation of PoweredUp sensors on microcontrollers
+ * Copyright (C) 2021 Ysard - <ysard@users.noreply.github.com>
+ *
+ * Based on the original work of Ahmed Jouirou - <ahmed.jouirou@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef BASICSENSOR_H
 #define BASICSENSOR_H
 
