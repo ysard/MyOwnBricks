b86714a756b5304249b169239f62aef1226ad28e
ysard <ysard@users.noreply.github.com>
2021-12-14 18:24:55 +0100

    Refactor: move useful color detection file to utilities/ dir; Add main header for the lib

---
+++ src/MyOwnBricks.h
@@ -0,0 +1,12 @@
+/**
+ * @brief Public API to the library.
+ */
+
+#ifndef MyOwnBricks_h
+#define MyOwnBricks_h
+
+#include "ColorDistanceSensor.h"
+#include "TiltSensor.h"
+#include "utilities/color_detection_methods.hpp"
+
+#endif // MyOwnBricks_h
