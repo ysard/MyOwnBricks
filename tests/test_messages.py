16385f0048c5f7a5b54b3ca2efec177e32d1cccd
ysard <ysard@users.noreply.github.com>
2023-04-15 01:56:22 +0200

    python tests: cover more cases for forge_write_mode_msg()

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -25,9 +25,22 @@ def test_forge_mode_msg():
 
 
 def test_forge_write_mode_msg():
+    # Simulate Mode 5 (set RGB color ID) for Color & Distance sensor
     found = forge_write_mode_msg(5, data=0x9)
     expected = bytearray(b"\x46\x00\xb9\xc5\x09\xcc")
     assert found == expected
 
-    with pytest.raises(AssertionError):
-        forge_write_mode_msg(0)
+    # Simulate Mode 7 (set IR code) for Color & Distance sensor
+    found = forge_write_mode_msg(7, data=0x375e)
+    expected = bytearray(b"\x46\x00\xb9\xcf\x5e\x37\xa6")
+    assert found == expected
+
+    # Simulate random extended mode (value >= 8)
+    found = forge_write_mode_msg(13, data=0x9)
+    expected = bytearray(b"\x46\x08\xb1\xc5\x09\xcc")
+    assert found == expected
+
+    # Demonstrate default data param 0x04
+    found = forge_write_mode_msg(5)
+    expected = bytearray(b"\x46\x00\xb9\xc5\x04\xc1")
+    assert found == expected
